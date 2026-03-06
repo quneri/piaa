b8cd6ce373bb3ad2708a541fcfbf6ee58477c3c4
quneri <romahin06@mail.ru>
2026-03-06 18:02:03 +0300

    first optimization without success

--- task_1.py
+++ task_1.py
@@ -1,15 +1,19 @@
 def solve(n):
+    board = [[0] * n for _ in range(n)]
     best_solution = []
     best_count = float('inf')
 
-    def find_empty_cell(board):
+    solution = []
+    stack = []
+
+    def find_empty_cell():
         for i in range(n):
             for j in range(n):
                 if board[i][j] == 0:
                     return i, j
         return None
 
-    def can_place_square(board, x, y, size):
+    def can_place_square(x, y, size):
         if x + size > n or y + size > n:
             return False
         for i in range(x, x + size):
@@ -18,36 +22,51 @@ def solve(n):
                     return False
         return True
 
-    def place_square(board, x, y, size, value):
+    def place_square(x, y, size, value):
         for i in range(x, x + size):
             for j in range(y, y + size):
                 board[i][j] = value
 
-    empty_board = [[0] * n for _ in range(n)]
-    stack = [([], empty_board)]
+    empty = find_empty_cell()
+    if empty is None:
+        return []
+
+    x, y = empty
+    max_size = min(n - x, n - y, n - 1)
+    stack.append([x, y, max_size])
 
     while stack:
-        current_solution, current_board = stack.pop()
+        x, y, size = stack[-1]
 
-        if len(current_solution) >= best_count:
+        if size == 0:
+            stack.pop()
+            if solution:
+                px, py, ps = solution.pop()
+                place_square(px, py, ps, 0)
             continue
 
-        empty = find_empty_cell(current_board)
-        if not empty:
-            best_solution = current_solution
-            best_count = len(current_solution)
+        stack[-1][2] -= 1
+        if len(solution) >= best_count:
             continue
 
-        x, y = empty
-        max_size = min(n - x, n - y, n - 1)
+        if not can_place_square(x, y, size):
+            continue
 
-        for size in range(max_size, 0, -1):
-            if can_place_square(current_board, x, y, size):
-                new_board = [row[:] for row in current_board]
-                place_square(new_board, x, y, size, 1)
+        place_square(x, y, size, 1)
+        solution.append((x, y, size))
 
-                new_solution = current_solution + [(x, y, size)]
-                stack.append((new_solution, new_board))
+        empty = find_empty_cell()
+        if empty is None:
+            if len(solution) < best_count:
+                best_count = len(solution)
+                best_solution = solution[:]
+
+            px, py, ps = solution.pop()
+            place_square(px, py, ps, 0)
+        else:
+            nx, ny = empty
+            max_size = min(n - nx, n - ny, n - 1)
+            stack.append([nx, ny, max_size])
 
     return best_solution
 
@@ -58,4 +77,3 @@ solution = solve(n)
 print(len(solution))
 for x, y, w in solution:
     print(x + 1, y + 1, w)
-
