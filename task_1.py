424e9d67a2c6dd6069ab90dfc7b2df37fd407acc
quneri <romahin06@mail.ru>
2026-03-11 12:59:54 +0300

    3rd optimization with heights without success

--- task_1.py
+++ task_1.py
@@ -1,83 +1,48 @@
 def solve(n):
-    board = [[0] * n for _ in range(n)]
-    best_solution = []
-    best_count = float('inf')
+    heights = [0] * n
+    best_solution = None
+    best_count = n ** 2
 
-    solution = []
-    stack = []
-
-    def find_empty_cell():
-        for i in range(n):
-            for j in range(n):
-                if board[i][j] == 0:
-                    return i, j
-        return None
-
-    def can_place_square(x, y, size):
-        if x + size > n or y + size > n:
-            return False
-        for i in range(x, x + size):
-            for j in range(y, y + size):
-                if board[i][j] != 0:
-                    return False
-        return True
-
-    def place_square(x, y, size, value):
-        for i in range(x, x + size):
-            for j in range(y, y + size):
-                board[i][j] = value
-
-    empty = find_empty_cell()
-    if empty is None:
-        return []
-
-    x, y = empty
-    max_size = min(n - x, n - y, n - 1)
-    stack.append([x, y, max_size])
+    stack = [(heights, [])]
 
     while stack:
-        x, y, size = stack[-1]
+        heights, solution = stack.pop()
 
-        if size == 0:
-            stack.pop()
-            if solution:
-                px, py, ps = solution.pop()
-                place_square(px, py, ps, 0)
+        if len(solution) >= best_count:
             continue
 
-        stack[-1][2] -= 1
-        if len(solution) + 1 >= best_count:
+        min_height = min(heights)
+        index = heights.index(min_height)
+
+        if min_height == n:
+            best_count = len(solution)
+            best_solution = solution[:]
             continue
 
-        if not can_place_square(x, y, size):
-            continue
+        max_size = min(n - min_height, n - index, n - 1)
+        while max_size > 0:
+            flag = True
+            for i in range(index, index + max_size):
+                if heights[i] != min_height:
+                    flag = False
+                    break
 
-        place_square(x, y, size, 1)
-        solution.append((x, y, size))
+            if flag:
+                break
+            max_size -= 1
 
-        empty = find_empty_cell()
-        if empty is None:
-            if len(solution) < best_count:
-                best_count = len(solution)
-                best_solution = solution[:]
+        for size in range(max_size, 0, -1):
+            new_heights = heights[:]
+            for i in range(index, index + size):
+                new_heights[i] += size
 
-            px, py, ps = solution.pop()
-            place_square(px, py, ps, 0)
-        else:
-            nx, ny = empty
-            max_size = min(n - nx, n - ny, n - 1)
-
-            while max_size > 0 and not can_place_square(nx, ny, max_size):
-                max_size -= 1
-
-            stack.append([nx, ny, max_size])
-
+            new_solution = solution + [(index, min_height, size)]
+            stack.append((new_heights, new_solution))
     return best_solution
 
 
 n = int(input())
 solution = solve(n)
-
 print(len(solution))
 for x, y, w in solution:
     print(x + 1, y + 1, w)
